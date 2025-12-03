1cce301685382f0e91e0a3809d9f492a2a748fb3
john.markson <john.markson@itm-development.com>
2025-12-04 01:10:38 +0300

    final change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "./components/ui/toaster";
-import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-      <Analytics />
     </>
   );
 }
