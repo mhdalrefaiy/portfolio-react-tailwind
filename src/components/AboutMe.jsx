03d2d919aac3351a5a6465ee9534482f9b90a02f
john.markson <john.markson@itm-development.com>
2025-12-14 14:33:10 +0300

    change cv pathname

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/mhd-rf-cv-updated.pdf"
+                href="/mhd-alrefaiy-cv.pdf"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
